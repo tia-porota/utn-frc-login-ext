202f598b1366c0314c782b6d03d34b16163826fb
tia-porota <cafesinleche935@gmail.com>
2025-06-11 13:13:54 -0300

    fix selector. add hyperlink. addcredits

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,15 @@
 let selector = document.getElementById("extdomains");
 selector.addEventListener("change", async (e) => {
-    setValue(e.target.value)
+  setValue(e.target.value);
 });
 async function setValue(value) {
-    await browser.storage.local.set({ value });
-
+  await chrome.storage.local.set({ value });
 }
 
-async function getValue(){
-    const result = await browser.storage.local.get("value");
-    selector.value = !result ? "sistemas" : result.value;
-    
-
+async function getValue() {
+  const result = await chrome.storage.local.get("value");
+  selector.value = !result.value ? "sistemas" : result.value;
+  
 }
 
-getValue()
+getValue();
